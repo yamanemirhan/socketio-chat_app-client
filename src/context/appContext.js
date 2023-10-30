9555c35ae4a0ff48f675568fe4ebfb3b35b2fd35
Emirhan Yaman <90368997+yamanemirhan@users.noreply.github.com>
2023-10-30 23:03:36 +0300

    Update appContext.js

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { io } from "socket.io-client";
-const SOCKET_URL = "http://localhost:4000";
+const SOCKET_URL = "https://socketio-chat-app-nskb.onrender.com";
 export const socket = io(SOCKET_URL);
 // app context
 export const AppContext = React.createContext();
