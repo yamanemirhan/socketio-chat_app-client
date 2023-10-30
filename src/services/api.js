116d3f968c06ab30876074bb639dc8a034ba5cca
Emirhan Yaman <90368997+yamanemirhan@users.noreply.github.com>
2023-10-30 23:02:37 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:4000",
+    baseUrl: "https://socketio-chat-app-nskb.onrender.com",
   }),
 
   endpoints: (builder) => ({
